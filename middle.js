7b69234f68b42d80d1d8e538f346e66c476a41a7
Ethan <ethan.steip@gmail.com>
2022-11-16 16:00:59 -0500

    formatting comments

--- middle.js
+++ middle.js
@@ -66,21 +66,22 @@ const middle = function(arr) {
 
   // save length of the array to variable
   const arrLength = arr.length;
+  // find the middle index of the array
   const findMiddle = Math.floor(arrLength / 2);
+  //initialize new array that will hold middle element(s)
   const middleElements = [];
 
-  // check if the array holds 2 or less, elements. If true return empty array.
+  // check if the array holds 2 or less elements. If true, return empty array.
   if (arrLength <= 2) {
-    //middleElements = [];
     console.log(middleElements);
     return middleElements;
   }
-  // Check if the array holds an odd number of elements. Push middle element to new array if it does.
+  // Check if the array holds an odd number of elements. If true, push middle element to new array.
   if (arr.length % 2 !== 0) {
     // console.log(arr[findMiddle]);
     middleElements.push(arr[findMiddle]);
   }
-  // Check if the array holds an even number of elements. Push middle elements to new array if it does.
+  // Check if the array holds an even number of elements. If true, push middle elements to new array.
   if (arr.length % 2 === 0) {
     // console.log(arr[Math.floor(arrLength / 2)]);
     middleElements.push(arr[findMiddle - 1], arr[findMiddle]);
